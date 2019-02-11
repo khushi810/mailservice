c0308b731d16cc603ae193646d2ee289ea7a3958
Khushali Vaghani <vaghanikhushali17@gmail.com>
2019-02-11 23:24:43 +0530

    Send mail via SMTP

--- src/emails/forms.py
+++ src/emails/forms.py
@@ -2,11 +2,21 @@ from django import forms
 
 from .models import Email
 
-class EmailForm(forms.ModelForm):
-  class Meta:
-    model = Email
-    fields = [
-        'email_to',
-        'subject',
-        'body'
-    ]
+class EmailForm(forms.Form):
+    email_to      = forms.EmailField(required=True)
+    subject       = forms.CharField()
+    body          = forms.CharField(widget=forms.Textarea)
+
+    # body          = forms.CharField(
+    #                   max_length=2000,
+    #                   widget=forms.Textarea()
+    #                 )
+
+# class EmailForm(forms.ModelForm):
+#   class Meta:
+#     model = Email
+#     fields = [
+#         'email_to',
+#         'subject',
+#         'body'
+#     ]
