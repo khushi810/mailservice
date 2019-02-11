3fdaa7bf7aef05cc94111526ec942828961805e3
Khushali Vaghani <vaghanikhushali17@gmail.com>
2019-02-12 01:06:01 +0530

    Add CC and BCC in model

--- src/emails/models.py
+++ src/emails/models.py
@@ -7,11 +7,12 @@ class Admin(models.Model):
   password    = models.CharField(max_length=50)
 
 class Email(models.Model):
-  email_to    = models.CharField(max_length=120)
+  email_to      = models.CharField(max_length=120)
+  cc            = models.CharField(max_length=120, null=True)
+  bcc           = models.CharField(max_length=120, null=True)
   subject       = models.CharField(max_length=120)
   body          = models.TextField()
-  #cc            = models.CharField(max_length=120)
-  #bcc           = models.CharField(max_length=120)
+
   #emailid_from  = models.CharField(max_length=120)
 
 #class CSV(models.Model):
--- src/emails/views.py
+++ src/emails/views.py
@@ -1,22 +1,13 @@
 from django.shortcuts import render
 from django.conf import settings
 from django.http import HttpResponse
-from django.core.mail import send_mail, BadHeaderError
+from django.core.mail import send_mail, BadHeaderError, EmailMessage
 from .models import Email
 from .forms import EmailForm
 
 # Create your views here.
 
 def email_view(request):
-     #  my_form = EmailForm(request.POST or None)
-     #  my_form = my_form.save(commit=False)
-     # # if my_form.is_valid():
-     #   # my_form.save()
-
-     #  subject = my_form.subject
-     #  message = my_form.body
-     #  from_email = settings.EMAIL_HOST_USER
-     #  to_list = [my_form.email_to]
 
     my_form = EmailForm(request.POST or None)
     if my_form.is_valid():
@@ -24,10 +15,12 @@ def email_view(request):
       message = my_form.cleaned_data['body']
       from_email = settings.EMAIL_HOST_USER
       email_id = my_form.cleaned_data['email_to']
-      #print(email_id)
       to_list = [email_id]
+      my_form.save()
+
       try:
         send_mail(subject, message, from_email, to_list, fail_silently=True)
+        my_form = EmailForm()
       except BadHeaderError:
         return HttpResponse('Invalid header found.')
     context = {
