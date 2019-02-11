2689a907c186a9a23f266b7448c59468dd3a5025
Khushali Vaghani <vaghanikhushali17@gmail.com>
2019-02-11 12:44:54 +0530

    Add Admin and Email model in to emails app

--- src/emails/admin.py
+++ src/emails/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Admin
+from .models import Email
+#from .models import CSV
+
+admin.site.register(Admin)
+admin.site.register(Email)
+#admin.site.register(CSV)
