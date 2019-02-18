ce29053cd361b8f28e7ed5f8e3e523d06a753d48
Khushali Vaghani <vaghanikhushali17@gmail.com>
2019-02-19 01:43:23 +0530

    Add created_at and email_at to record email sent timestamp

--- src/emails/models.py
+++ src/emails/models.py
@@ -10,5 +10,13 @@ class Email(models.Model):
   bcc           = models.CharField(max_length=120, null=True)
   subject       = models.CharField(max_length=120)
   body          = models.TextField()
+  created_at    = models.DateTimeField(editable=False)
+  updated_at    = models.DateTimeField()
 
+  def save(self, *args, **kwargs):
+    ''' On save, update timestamps '''
+    if not self.id:
+      self.created = timezone.now()
+    self.modified = timezone.now()
+    return super(User, self).save(*args, **kwargs)
 
